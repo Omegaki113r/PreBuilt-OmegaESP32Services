bbd57ba1a2f1a4c739e0f5385e3f26378b066926
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-28 02:09:51 +0530

    hash controller done

--- OmegaHashController/OmegaHashController.h
+++ OmegaHashController/OmegaHashController.h
@@ -1,3 +1,19 @@
+/*
+ * Project: OmegaESP32Services
+ * File Name: OmegaHashController.h
+ * File Created: Thursday, 22nd February 2024 4:42:02 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Wednesday, 28th February 2024 1:36:38 am
+ * Modified By: Omegaki113r (omegaki113r@gmail.com>)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
 #ifndef __OMEGA_HASH_CONTROLLER__
 #define __OMEGA_HASH_CONTROLLER__
 
@@ -9,14 +25,32 @@ extern "C"
 #include <stdio.h>
 #include <string.h>
 
+#include <mbedtls/md.h>
 #include <mbedtls/sha256.h>
+
+    typedef enum
+    {
+        HSC_SUCCESS,
+        HSC_FAILED,
+        HSC_INVALID_PARAMETERS,
+        HSC_HASH_ALGO_NOT_FOUND,
+        HSC_NO_MEM,
+        HSC_UNKNOWN
+    } HashControllerStatus;
+
+    typedef enum
+    {
+        HASH256,
+    } HashAlgorithm;
 
     typedef struct
     {
-        mbedtls_sha256_context ctx;
+        mbedtls_md_context_t ctx;
     } OmegaHashController_t;
 
-    void OmegaHashController_reset_module(OmegaHashController_t *);
+    HashControllerStatus OmegaHashController_init(OmegaHashController_t *, HashAlgorithm);
+    HashControllerStatus OmegaHashController_ingest_data_single(OmegaHashController_t *, const uint8_t *, const size_t, uint8_t *);
+    HashControllerStatus OmegaHashController_ingest_data_streamed(OmegaHashController_t *, const uint8_t *, const size_t, uint8_t *out_buffer);
 
 #ifdef __cplusplus
 }
