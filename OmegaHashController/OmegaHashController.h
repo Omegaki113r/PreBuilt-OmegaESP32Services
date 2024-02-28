a8788221a343f592b779bf1eb0e99f38ce8fc3e0
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-29 01:33:19 +0530

    md reset added

--- OmegaHashController/OmegaHashController.h
+++ OmegaHashController/OmegaHashController.h
@@ -4,7 +4,7 @@
  * File Created: Thursday, 22nd February 2024 4:42:02 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Wednesday, 28th February 2024 1:36:38 am
+ * Last Modified: Thursday, 29th February 2024 1:28:47 am
  * Modified By: Omegaki113r (omegaki113r@gmail.com>)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -22,9 +22,9 @@ extern "C"
 {
 #endif
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 
 #include <mbedtls/md.h>
 #include <mbedtls/sha256.h>
@@ -50,6 +50,7 @@ extern "C"
     } OmegaHashController_t;
 
     HashControllerStatus OmegaHashController_init(OmegaHashController_t *, HashAlgorithm);
+    HashControllerStatus OmegaHashController_reset(OmegaHashController_t *);
     HashControllerStatus OmegaHashController_ingest_data_single(OmegaHashController_t *, const uint8_t *, const size_t, uint8_t *);
     HashControllerStatus OmegaHashController_ingest_data_streamed(OmegaHashController_t *, const uint8_t *, const size_t, uint8_t *out_buffer);
 
